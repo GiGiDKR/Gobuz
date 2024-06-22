5888bfb27694c742071863987bf26d5439cf122d
Gildas <74984020+GiGiDKR@users.noreply.github.com>
2024-06-22 20:47:33 +0200

    v0.3

--- qobuz_dl/login.py
+++ qobuz_dl/login.py
@@ -1,8 +1,10 @@
-import tkinter as tk
-from ttkbootstrap import ttk
+import ttkbootstrap as ttk
+from ttkbootstrap.dialogs import Messagebox
 
+def clear_placeholder(event):
+    event.widget.delete(0, 'end')
 
-class Login(tk.Toplevel):
+class Login(ttk.Window):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.init_view()
@@ -11,21 +13,33 @@ class Login(tk.Toplevel):
         self.title('Qobuz Login')
         self.geometry('400x120')
 
-        self.text_email = ttk.Entry(self)
-        self.text_email.set_placeholder('Enter your e-mail')
+        # Création d'un cadre pour organiser les widgets
+        layout = ttk.Frame(self)
+        layout.grid(padx=10, pady=10)
 
-        self.text_pass = ttk.Entry(self)
-        self.text_pass.set_placeholder('Enter your password')
-        self.text_pass.set_show('*')  # to hide the password
+        self.text_email = ttk.Entry(layout, bootstyle='info')
+        self.text_email.insert(0, 'Enter your e-mail')
+        self.text_email.bind("<FocusIn>", clear_placeholder)
+        self.text_email.grid(row=0, column=0, padx=5, pady=5, sticky='ew')
 
-        self.btn_login = ttk.Button(self, text='Login', command=self.handle_login)
+        self.text_pass = ttk.Entry(layout, bootstyle='info', show='*')
+        self.text_pass.insert(0, 'Enter your password')
+        self.text_pass.bind("<FocusIn>", clear_placeholder)
+        self.text_pass.grid(row=1, column=0, padx=5, pady=5, sticky='ew')
 
-        self.text_email.pack(padx=10, pady=5, fill='x')
-        self.text_pass.pack(padx=10, pady=5, fill='x')
-        self.btn_login.pack(padx=10, pady=5)
+        self.btn_login = ttk.Button(layout, text='Login', command=self.handle_login, bootstyle='primary')
+        self.btn_login.grid(row=2, column=0, padx=5, pady=5, sticky='ew')
+
+        # Configuration de la colonne pour qu'elle s'étende avec la fenêtre
+        layout.columnconfigure(0, weight=1)
 
     def handle_login(self):
         if self.text_email.get() == "" or self.text_pass.get() == "":
-            ttk.messagebox.warning(self, 'Error', 'Login information is required')
+            Messagebox.show_warning('Error', 'Login information is required')
         else:
             self.destroy()
+
+# Exemple d'utilisation
+if __name__ == '__main__':
+    login = Login()
+    login.mainloop()
--- qobuz_dl/qdl-gui.py
+++ qobuz_dl/qdl-gui.py
@@ -1,106 +1,38 @@
 import os
 import configparser
-import time
-import tkinter
-from ttkbootstrap.constants import *
-from ttkbootstrap.publisher import Publisher
-from ttkbootstrap.style import Style
-from ttkbootstrap.icons import Icon
-from ttkbootstrap import utility
 import ttkbootstrap as ttk
 from tkinter import messagebox, Listbox
 from ttkbootstrap import Scrollbar, Progressbar
 from ttkbootstrap.scrolled import ScrolledFrame
 from ttkbootstrap.constants import *
 from ttkbootstrap.tooltip import ToolTip
+from ttkbootstrap import Style
 import logging
 import threading
-import hashlib
 import requests
-from time import sleep
-from os import getcwd
-from pathlib import Path
-from login import Login
+import hashlib
+import sys
+from datetime import datetime
 
-from qobuz_dl.bundle import Bundle
 from qobuz_dl.core import QobuzDL
-
-
-if os.name == "nt":
-    OS_CONFIG = os.environ.get("APPDATA")
-else:
-    OS_CONFIG = os.path.join(os.environ["HOME"], ".config")
-
-CONFIG_PATH = os.path.join(OS_CONFIG, "qobuz-dl")
-CONFIG_FILE = os.path.join(CONFIG_PATH, "config.ini")
-
-QOBUZ_DB = os.path.join(CONFIG_PATH, "qobuz_dl.db")
-
-def reset_config(config_file):
-    logging.info(f"{YELLOW}Creating config file: {config_file}")
-    config = configparser.ConfigParser()
-    config["Credentials"]["email"] = ""
-    config["Credentials"]["password"] = ""
-    config["Credentials"]["expirationdate"] = ""
-    config["DEFAULT"]["default_folder"] = "Qobuz Downloads"
-    config["DEFAULT"]["default_quality"] = "6"
-    config["DEFAULT"]["default_limit"] = "20"
-    config["DEFAULT"]["no_m3u"] = "false"
-    config["DEFAULT"]["albums_only"] = "false"
-    config["DEFAULT"]["no_fallback"] = "false"
-    config["DEFAULT"]["og_cover"] = "false"
-    config["DEFAULT"]["embed_art"] = "false"
-    config["DEFAULT"]["no_cover"] = "false"
-    config["DEFAULT"]["no_database"] = "false"
-    logging.info(f"{YELLOW}Getting tokens. Please wait...")
-    bundle = Bundle()
-    config["DEFAULT"]["app_id"] = str(bundle.get_app_id())
-    config["DEFAULT"]["secrets"] = ",".join(bundle.get_secrets().values())
-    config["DEFAULT"]["folder_format"] = DEFAULT_FOLDER
-    config["DEFAULT"]["track_format"] = DEFAULT_TRACK
-    config["DEFAULT"]["smart_discography"] = "false"
-    with open(config_file, "w") as configfile:
-        config.write(configfile)
-    logging.info(
-        f"{GREEN}Config file updated. Edit more options in {config_file}"
-        "\nso you don't have to call custom flags every time you run "
-        "a qobuz-dl command."
-    )
-
-def check_config_init():
-    if not os.path.isdir(CONFIG_PATH) or not os.path.isfile(CONFIG_FILE):
-        os.makedirs(CONFIG_PATH, exist_ok=True)
-        reset_config(CONFIG_FILE)
-
-def login(config, config_path):
-    login_window = Login()
-
-    try:
-        email = config["DEFAULT"]["email"]
-        password = config["DEFAULT"]["password"]
-    except (KeyError, UnicodeDecodeError, configparser.Error) as error:
-        logging.info(f"{RED}Your config file is corrupted: {error}!")
-        return
-
-    if email == "" or password == "":
-        if login_window.exec_() == QtWidgets.QDialog.Accepted:
-            email = login_window.text_email.text()
-            password = login_window.text_pass.text()
-
-            config["DEFAULT"]["email"] = email
-            config["DEFAULT"]["password"] = hashlib.md5(
-                password.encode("utf-8")).hexdigest()
-
-            with open(config_path, "w") as config_file:
-                config.write(config_file)
-
-    default_folder = config["DEFAULT"]["default_folder"]
-    default_limit = int(config["DEFAULT"]["default_limit"])
-    default_quality = int(config["DEFAULT"]["default_quality"])
-
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
+
+
+def resolve_path(file_name):
+    if getattr(sys, "frozen", False):
+        base_path = os.path.abspath(os.path.join(sys._MEIPASS))
+    else:
+        base_path = os.path.abspath(os.path.join(os.getcwd()))
+
+    config_dir = os.path.join(base_path, '.config')
+    if not os.path.exists(config_dir):
+        os.makedirs(config_dir)
+
+    resolved_path = os.path.join(config_dir, file_name)
+
+    return resolved_path
 
 
 class OptionsWindow:
@@ -109,79 +41,144 @@ class OptionsWindow:
         self.app_instance = app_instance
         self.parent.title("Options")
 
-        self.limit_label = ttk.Label(parent, text="Limit :")
-        self.limit_label.grid(row=0, column=0, padx=10, pady=10)
-        self.limit_spinbox = ttk.Spinbox(parent, from_=1, to=100, increment=1)
-        self.limit_spinbox.set(app_instance.limit)
-        self.limit_spinbox.grid(row=0, column=1, padx=10, pady=10)
+        self.email_label = ttk.Label(parent, text="Email :")
+        self.email_label.grid(row=0, column=0, padx=10, pady=10)
+        self.email_entry = ttk.Entry(parent, style='dark.TEntry')
+        self.email_entry.grid(row=0, column=1, padx=10, pady=10)
+
+        self.password_label = ttk.Label(parent, text="Password :")
+        self.password_label.grid(row=1, column=0, padx=10, pady=10)
+        self.password_entry = ttk.Entry(parent, style='dark.TEntry')
+        self.password_entry.grid(row=1, column=1, padx=10, pady=10)
+
+        #self.limit_label = ttk.Label(parent, text="Limit :")
+        #self.limit_label.grid(row=2, column=0, padx=10, pady=10)
+        ##self.limit_spinbox = ttk.Spinbox(parent, from_=1, to=100, increment=1)
+        #self.limit_spinbox.set(app_instance.limit)
+        #self.limit_spinbox.grid(row=2, column=1, padx=10, pady=10)
 
         self.submit_button = ttk.Button(parent, text="Save", command=self.save_options)
-        self.submit_button.grid(row=1, columnspan=2, padx=10, pady=10)
+        self.submit_button.grid(row=3, columnspan=2, padx=10, pady=10)
 
     def save_options(self):
-        self.app_instance.limit = int(self.limit_spinbox.get())
+        email = self.email_entry.get()
+        password = self.password_entry.get()
+
+        # Encodage du mot de passe en utilisant hashlib
+        hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+        # Création ou mise à jour du fichier de configuration
+        config = configparser.ConfigParser()
+        config.read(self.app_instance.config_path)  # Assurez-vous que config_path est défini dans app_instance
+        config['Credentials']['email'] = email
+        config['Credentials']['password'] = hashed_password
+        #self.app_instance.limit = int(self.limit_spinbox.get())
+
+        # Écriture des modifications dans le fichier 'config.ini'
+        with open(self.app_instance.config_path, 'w') as configfile:
+            config.write(configfile)
+
         messagebox.showinfo("Options Saved", "Options have been saved successfully.")
         self.parent.destroy()
 
 
+def update_credentials(resolved_path):
+    url = "https://raw.githubusercontent.com/GiGiDKR/qobuz-dl-gui/main/qobuz_dl/config.ini"
+    filename = resolved_path("config.ini")
+    r = requests.get(url)
+    f = open(filename, 'wb')
+    f.write(r.content)
+    #messagebox.showinfo("Credentials", "Up-to-date Qobuz credentials downloded")
+
+
 class QobuzDLApp:
-    def __init__(self, root, results):
+    def __init__(self, root, results, resolved_path):
         self.root = root
         self.results = results
         self.root.title("Qobuz Downloader")
 
-        # Load or initialize QobuzDL instance
         self.qobuz = QobuzDL()
 
-        # Default limit value
         self.limit = 20
 
-        # Create widgets for user input
         self.create_widgets()
 
-        # Load credentials from file or ask user
-        self.load_credentials()
+        self.load_credentials(resolve_path)
+        self.config_path = ".config\\config.ini"
+        self.preferences_file = resolved_path("preferences.ini")
+        self.config_file = resolved_path("config.ini")
+        self.config = configparser.ConfigParser()
+        self.load_config()
+        self.load_user_preferences()
 
-    def user_update_credentials(self):
+    def load_config(self):
+        # Charger le fichier de configuration
+        self.config.read(self.config_file)
+
+    def place_window_center(self):
+        self.root.update_idletasks()
+        w_height = self.root.winfo_height()
+        w_width = self.root.winfo_width()
+        s_height = self.root.winfo_screenheight()
+        s_width = self.root.winfo_screenwidth()
+        xpos = (s_width - w_width) // 2
+        ypos = (s_height - w_height) // 2
+        self.root.geometry(f'+{xpos}+{ypos}')
+
+    def user_update_credentials(self, resolved_path):
         url = "https://raw.githubusercontent.com/GiGiDKR/qobuz-dl-gui/main/qobuz_dl/config.ini"
-        filename = os.path.join(getcwd(), 'config.ini')
+        filename = resolved_path("config.ini")
         r = requests.get(url)
         f = open(filename, 'wb')
         f.write(r.content)
-        sleep(5)
-        messagebox.showinfo("Credentials", "The credentials are up-to-date")
+        self.load_credentials(resolve_path)
 
-    def load_credentials(self):
-        path_config = str(Path(__file__).resolve().with_name("config.ini"))
-        self.credentials_file = path_config
+    def load_credentials(self, resolved_path):
+        path_config = resolved_path("config.ini")
         config = configparser.ConfigParser()
-        config.read(self.credentials_file)
+        config.read(path_config)
         if "Credentials" in config:
             self.email = config["Credentials"].get("email", "")
             self.password = config["Credentials"].get("password", "")
             self.expiration_date = config["Credentials"].get("expirationdate", "")
             self.update_email_date_label()
         else:
-            self.update_credentials()
-
-    def update_credentials(self):
-        url = "https://raw.githubusercontent.com/GiGiDKR/qobuz-dl-gui/main/qobuz_dl/config.ini"
-        filename = os.path.join(getcwd(), 'config.ini')
-        r = requests.get(url)
-        f = open(filename, 'wb')
-        f.write(r.content)
-        messagebox.showinfo("Credentials", "Up-to-date Qobuz credentials downloded")
+            update_credentials(resolve_path)
 
     def update_email_date_label(self):
-        # Update email label with loaded email and expiration date
         email_label_text = f"{self.email}"
         self.email_label.config(text=email_label_text)
-        date_label_text = f"{self.expiration_date}"
+
+        expirationdate = datetime.strptime(self.expiration_date, "%d/%m/%Y")
+        dateactual = datetime.now()
+        difference = expirationdate - dateactual
+        date_label_text = f"{difference.days} days before expiration"
         self.date_label.config(text=date_label_text)
+
+    def load_user_preferences(self):
+        config = configparser.ConfigParser()
+
+        if not self.preferences_file:
+            config['Preferences'] = {
+                'theme': 'superhero',
+            }
+            with open(self.preferences_file, 'w') as configfile:
+                config.write(configfile)
+        else:
+            config.read(self.preferences_file)
+            theme = config.get('Preferences', 'theme', fallback='superhero')
+            self.change_theme(theme)
+
+    def save_user_preferences(self, theme_name):
+        config = configparser.ConfigParser()
+        config['Preferences'] = {
+            'theme': theme_name,
+        }
+        with open(self.preferences_file, 'w') as configfile:
+            config.write(configfile)
 
     def create_widgets(self):
 
-        # Search type selection
         self.search_type_label = ttk.Label(self.root, text="Search Type :")
         self.search_type_label.grid(row=0, column=0, padx=10, pady=10)
         self.search_type_combobox = ttk.Combobox(self.root, style='dark.TCombobox',
@@ -191,14 +188,12 @@ class QobuzDLApp:
         ToolTip(self.search_type_combobox, text="Download albums, tracks, artists, playlists and labels",
                                                 bootstyle="info")
 
-        # Search query input
         self.search_query_label = ttk.Label(self.root, text="Search :")
         self.search_query_label.grid(row=1, column=0, padx=10, pady=10)
         self.search_query_entry = ttk.Entry(self.root, style='dark.TEntry')
         self.search_query_entry.grid(row=1, column=1, padx=10, pady=10)
         ToolTip(self.search_query_entry, text="Enter your search", bootstyle="info")
 
-        # Slider
         current_value = ttk.IntVar(value=20)
 
         def get_current_value():
@@ -208,7 +203,6 @@ class QobuzDLApp:
             value_label.configure(text=get_current_value())
             self.limit = current_value.get()
 
-        # value label
         limit_label = ttk.Label(text='Limit : ')
         limit_label.grid(row=3, columnspan=1, padx=10, pady=10)
 
@@ -223,18 +217,15 @@ class QobuzDLApp:
         self.slider.grid(row=3, columnspan=2, padx=10, pady=10)
         ToolTip(self.slider, text="Limit of search results (1>100)", bootstyle="info")
 
-        # value label
         value_label = ttk.Label(
             text=get_current_value()
         )
         value_label.grid(row=3, columnspan=2, padx=10, pady=10)
 
-        # Start search button
         self.search_button = ttk.Button(self.root, text="Search", bootstyle="toolbutton, outline", takefocus=1,
                                         command=self.start_search)
         self.search_button.grid(row=4, columnspan=2, padx=10, pady=10)
 
-        # Quality selection
         self.quality_label = ttk.Label(self.root, text="Quality :")
         self.quality_label.grid(row=2, column=0, padx=10, pady=10)
         self.quality_combobox = ttk.Combobox(self.root, style='dark.TCombobox', values=list(Qualities.values()))
@@ -242,11 +233,9 @@ class QobuzDLApp:
         self.quality_combobox.current(1)  # Default to "6 - 16 bit, 44.1kHz"
         ToolTip(self.quality_combobox, text="Select audio quality for download", bootstyle="warning")
 
-        # Results listbox with scrollbar
         self.results_listbox = ScrolledFrame(self.root, autohide=True)
         self.results_listbox.grid(row=5, columnspan=2, padx=10, pady=10)
 
-        # Download selected button
         self.download_button = ttk.Checkbutton(self.root, text="Download Selected", bootstyle="success-toolbutton",
                                                takefocus=1, command=self.download_selected)
 
@@ -258,36 +247,44 @@ class QobuzDLApp:
         #sizegrip = ttk.Sizegrip(bootstyle="light")
         #sizegrip.grid(row=8, columnspan=2, sticky="e")
 
-        # Email label
         self.email_label = ttk.Label(self.root, text="", bootstyle="light")
         self.email_label.grid(row=8, columnspan=1, padx=10, pady=10, sticky="w")
         self.date_label = ttk.Label(self.root, text="", bootstyle="light")
         self.date_label.grid(row=8, columnspan=2, padx=10, pady=10, sticky="e")
 
-        # Create menu
+        self.search_query_entry.bind("<Return>", lambda event: self.start_search())
+
         self.create_menu()
 
     def create_menu(self):
         menu_bar = ttk.Menu(self.root)
         self.root.config(menu=menu_bar)
-
         file_menu = ttk.Menu(menu_bar, tearoff=0)
-        file_menu.add_command(label="Reset Credentials", command=self.load_credentials)
-        file_menu.add_command(label="Update Credentials", command=self.user_update_credentials)
+        #file_menu.add_command(label="Reset Credentials", command=self.load_credentials(resolve_path))
+        file_menu.add_command(label="Update Credentials", command=self.user_update_credentials(resolve_path))
+        file_menu.add_separator()
         file_menu.add_command(label="Options", command=self.show_options)
         file_menu.add_separator()
         file_menu.add_command(label="Exit", command=self.quit_application)
         menu_bar.add_cascade(label="File", menu=file_menu)
+        theme_menu = ttk.Menu(menu_bar, tearoff=0)
+        for theme in style.theme_names():
+            theme_menu.add_command(label=theme, command=lambda t=theme: self.change_theme(t))
+        menu_bar.add_cascade(label="Themes", menu=theme_menu)
 
     def quit_application(self):
         self.root.quit()
+
+    def change_theme(self, theme_name):
+        style.theme_use(theme_name)
+        self.save_user_preferences(theme_name)
 
     def show_options(self):
         self.options_window = ttk.Toplevel(self.root)
         OptionsWindow(self.options_window, self)
 
     def start_search(self):
-        self.load_credentials()
+        #self.load_credentials(resolve_path)
         search_type = self.search_type_combobox.get()
         query = self.search_query_entry.get()
 
@@ -304,8 +301,6 @@ class QobuzDLApp:
                 for result in results:
                     ttk.Checkbutton(self.results_listbox, bootstyle="round-toggle",
                                     text=f"{result['text']}").pack(anchor=W)
-
-                   # self.results_listbox.insert("end", result['text'])
                 self.search_results = results
             else:
                 messagebox.showinfo("No Results", "No results found for the given query.")
@@ -323,7 +318,7 @@ class QobuzDLApp:
             messagebox.showwarning("Selection Error", "Please select at least one item to download.")
             return
 
-        quality = self.quality_combobox.current() + 5  # Mapping to the quality values
+        quality = self.quality_combobox.current() + 5
         self.qobuz.quality = quality
 
         selected_items = [self.search_results[i] for i in selected_indices]
@@ -346,6 +341,7 @@ class QobuzDLApp:
             self.update_progressbar_complete()
 
 
+
 Qualities = {
     5: "MP3",
     6: "16 bit, 44.1kHz",
@@ -353,24 +349,11 @@ Qualities = {
     27: "24 bit, >96kHz",
 }
 
-
-def place_window_center(self):
-    self.update_idletasks()
-    w_height = self.winfo_height()
-    w_width = self.winfo_width()
-    s_height = self.winfo_screenheight()
-    s_width = self.winfo_screenwidth()
-    xpos = (s_width - w_width) // 2
-    ypos = (s_height - w_height) // 2
-    self.geometry(f'+{xpos}+{ypos}')
-
-
-position_center = place_window_center
-
 if __name__ == "__main__":
-    root = ttk.Window(themename="superhero", alpha=1, size=(330, 570))
-    root.position_center()
-    results = [...]  # Définir vos résultats ici
-    app = QobuzDLApp(root, results)
-    root.position_center()
+    style = Style()
+    root = style.master
+    results = [...]
+    app_instance = QobuzDLApp(root, results, resolve_path)
+    #app = QobuzDLApp(root, results, resolve_path)
+    app_instance.place_window_center()
     root.mainloop()
